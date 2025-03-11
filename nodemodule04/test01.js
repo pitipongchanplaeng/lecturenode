122cdeade82efe7aa7905927cd1e2a25474fc819
pitipongchanplaeng <S6419410030@sau.ac.th>
2025-03-11 12:40:11 +0700

    lecturenode esmodule

---
+++ nodemodule04/test01.js
@@ -0,0 +1,8 @@
+import {
+  calSquareArea,
+  calTriangleArea,
+  calCircleArea,
+} from "nin-calculate-area";
+console.log(`พท.สี่เหลี่ยมกว้าง 10 ยาว 20 คือ ${calSquareArea(10, 20)}`);
+console.log(`พท.สามเหลี่ยมสูง 5 ฐาน 220 คือ ${calTriangleArea(5, 220)}`);
+console.log(`พท.วงกลมรัศมี 50 คือ ${calCircleArea(50)}`);
